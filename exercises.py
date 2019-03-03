6925a9f1d8a5da35aba10edab6b44cf01a58293f
David <cichy.dawid@gmail.com>
2019-03-03 20:24:06 +0100

    SI05/03 Exercises: poker_hand _logic module made

--- exercises.py
+++ exercises.py
@@ -1,19 +1,4 @@
-'''
-Here are some exercises. Try to write a function header and doctests first, and only then implement the function.
-
-1/ Check what is the most frequent number in an array. If there are multiple such numbers, return the largest one. For instance, most_frequent([3, 3, 3, 2, 2, 2, 4, 4]) should return 3.
-2/ Calculate a cyclic rotation of a string; i.e. move the last N elements from the end to the beginning. For example, cyclic_rotation('abcde', 2) should return 'deabc'.
-3/ Write a poker_hand function that will score a poker hand. The function will take an array 5 numbers and return a string based on what is inside. It should recognize the following patterns:
-
-Name 	Description 	Example
-five 	five of a kind 	[1, 1, 1, 1, 1]
-four 	four of a kind 	[2, 2, 2, 2, 3]
-three 	three of a kind 	[1, 1, 1, 2, 3]
-twopairs 	two pairs 	[2, 2, 3, 3, 4]
-pair 	a single pair 	[1, 2, 2, 3, 4]
-fullhouse 	a pair and a three 	[1, 1, 2, 2, 2]
-nothing 	none of the above 	[1, 2, 3, 4, 6]
-'''
+import poker_hand_logic
 
 
 def most_frequent_number_in_array(array):
@@ -76,12 +61,6 @@ def cyclic_rotation(input_string, rotation):
     return new_string
 
 
-def poker_hand_check_if_five(cards_on_hand):
-    for i in range(5): 
-        if cards_on_hand [i] != cards_on_hand[0]:
-            return False
-    return True
-
 def poker_hand(cards_on_hand):
     '''
     Write a poker_hand function that will score a poker hand. 
@@ -109,7 +88,8 @@ def poker_hand(cards_on_hand):
     >>> poker_hand([1, 2, 3, 4, 6])
     'nothing'
     '''
-    if poker_hand_check_if_five(cards_on_hand):
+
+    if poker_hand_logic.check_if_five(cards_on_hand):
         return 'five'
     else:
         return 'nothing'
